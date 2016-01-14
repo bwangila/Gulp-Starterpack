bf640e07c4c18965250024d55185f907e837f653
Brian Wangila <brian@bwangila.com>
2016-01-14 20:24:38 +0300

    added asset variables

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,16 @@
+/*==========================================================
+ * Quick Settings
+ *=========================================================*/
+// Set you main source assets folder path (relative to this file) without a trailing slash
+var assets_source = 'assets/_src';
+
+// Set you main source build folder path (where you want compiled files to be stored)
+var assets_source = 'assets/_build';
+
+
+/*==========================================================
+ * Require Gulp & its plugins
+ *=========================================================*/
 // Include Gulp
 var gulp = require('gulp');
 
