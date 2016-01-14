7d3197bbbe4c8e5921bce702901ddbb00b9de5ed
Brian Wangila <brian@bwangila.com>
2016-01-14 20:19:52 +0300

    included gulp plugins

--- gulpfile.js
+++ gulpfile.js
@@ -1,2 +1,11 @@
 // Include Gulp
 var gulp = require('gulp');
+
+// Include Gulp plugins
+var concat = require('gulp-concat'),
+    cssnano = require('gulp-cssnano'),
+    autoprefixer = require('gulp-autoprefixer'),
+    uglify = require('gulp-uglify'),
+    jshint = require('gulp-jshint'),
+    imagemin = require('gulp-imagemin'),
+    pngquant = require('imagemin-pngquant');
